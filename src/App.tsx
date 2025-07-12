7cd09b9605f24add9f35466ef2a39c6542aadedf
ronaldAdd <ronald@importir.co>
2025-07-12 07:16:13 +0700

    update repo1

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 
+//FINAL
+
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
