146c3b92d938fe4dd5110819e55ce5665ce3d925
ronaldAdd <ronald@importir.co>
2025-07-14 21:04:53 +0700

    api onlyoffice

--- src/pages/New.tsx
+++ src/pages/New.tsx
@@ -28,7 +28,7 @@ export default function OnlyOfficeFileCreate() {
   const [loading, setLoading] = useState(false);
 
   const navigate = useNavigate();
-  const folderId = "1325204";
+  const folderId = "1332163";
 
   const createNewFile = async () => {
     setLoading(true);
